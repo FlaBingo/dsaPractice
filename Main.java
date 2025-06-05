544566a28cba9276dc0ed8f300662a2cbc6d98a0
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-05 12:01:36 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 9 };
+        int target = 10;
+        // System.err.println(binarySearch(arr, target));
+        System.err.println("greater than target " + ceiling(arr, target));
+        System.err.println("less than target " + floor(arr, target));
+    }
+
+
+
+    static int floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
+    }
+
+
+
+    // returns index of smallest number which is >= target
+    static int ceiling(int[] arr, int target) {
+
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+
+
+
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ findSmallest.java
@@ -0,0 +1,13 @@
+import javax.swing.plaf.synth.SynthSliderUI;
+
+public class findSmallest {
+    public static void main(String[] args) {
+        String[] arrofStrings = { "b", "e", "h", "k", "x" };
+        String target = "a";
+        System.err.println(func(arrofStrings, target));
+    }
+
+    static String func(String[] arrofStrings, String target) {
+        
+    }
+}
