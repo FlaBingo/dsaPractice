7083833bbc51370a99b43c6178e2ae85fd6cf2fd
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-06 19:04:53 +0530

    add: implement leet852 class for finding peak index in a mountain array

---
+++ leetcode/leet852.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+// Peak Index in a mountain Array [0, 2, 4, 2, 1, 0]
+public class leet852 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 5, 6, 4, 3, 2 };
+        System.out.println("Hello");
+    }
+
+    static int MaxNumIndex(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] > arr[mid + 1]){
+                start = mid + 1;
+            }
+        }
+    }
+}
