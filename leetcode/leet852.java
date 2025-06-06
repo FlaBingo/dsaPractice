145a6927a80047571d074e36e71b91c74916310b
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-06 19:43:13 +0530

    fix: update main method to print peak index instead of a greeting

--- leetcode/leet852.java
+++ leetcode/leet852.java
@@ -4,7 +4,7 @@ package leetcode;
 public class leet852 {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 5, 6, 4, 3, 2 };
-        System.out.println("Hello");
+        System.out.println(MaxNumIndex(arr));
     }
 
     static int MaxNumIndex(int[] arr){
@@ -15,8 +15,11 @@ public class leet852 {
             int mid = start + (end - start) / 2;
 
             if(arr[mid] > arr[mid + 1]){
+                end = mid - 1;
+            }else if(arr[mid] < arr[mid + 1]){
                 start = mid + 1;
             }
         }
+        return start;
     }
 }
