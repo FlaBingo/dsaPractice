5b97696db33554e23e2fbc814d9b38fe05560337
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-07 14:25:21 +0530

    fix: update comments in leet33.java to include example inputs and outputs

--- leetcode/leet33.java
+++ leetcode/leet33.java
@@ -1,6 +1,19 @@
 package leetcode;
 
 // Search in Rotated Sorted Array
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+// Example 3:
+
+// Input: nums = [1], target = 0
+// Output: -1
+
 public class leet33 {
     public static void main(String[] args) {
         // int[] nums = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
