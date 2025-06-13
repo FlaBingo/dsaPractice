b24c57a1a6df5807112acd518f84d75156697bfc
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-13 16:53:14 +0530

    refactor: update insertion sort implementation and remove unused selection sort code

--- sort_algorithms/insertion.java
+++ sort_algorithms/insertion.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class insertion {
     public static void main(String[] args) {
-        int[] arr = {23, 4, 43, 2, 3, 5};
+        int[] arr = {4, 5, 1, 2, 3};
         insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     };
@@ -12,40 +12,21 @@ public class insertion {
     static void insertionSort(int[] arr){
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
+                // System.out.println(i + "(i) is " + j + " less than " + (j-1));
                 if(arr[j] < arr[j-1]){
                     swap(arr, j, j-1);
                 }else{
+                    // System.out.println("no");
                     break;
                 }
+                // System.out.println(Arrays.toString(arr));
             }
         }
     }
-
-
-    static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            //find the max item in the ramaining array and swap with correct index
-            int last = arr.length - i - 1;
-
-            int maxIndex = getMaxIndex(arr, 0, last);
-
-            swap(arr, maxIndex, last);
-        }
-    };
 
     static void swap(int[] arr, int first, int second){
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
-    };
-
-    static int getMaxIndex(int[] arr, int start, int end){
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if(arr[max] < arr[i]){
-                max = i;
-            }
-        }
-        return max;
     };
 }
