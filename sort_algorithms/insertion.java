e0b3665aec21ba26d56374408699d17f1fe57ef9
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-13 16:14:05 +0530

    insertion sorting algorithm

---
+++ sort_algorithms/insertion.java
@@ -0,0 +1,17 @@
+package sort_algorithms;
+
+import java.util.Arrays;
+
+public class insertion {
+    public static void main(String[] args) {
+        int[] arr = {23, 4, 43, 2, 3, 5};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("hello");
+    }
+
+
+    static void insertionSort(int [] arr){
+        
+    };
+}
